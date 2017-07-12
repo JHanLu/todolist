f930fd726658f32ea8464ec51fe54869f3cf6634
JHanLu <864836681@qq.com>
2017-07-12 16:06:23 +0800

    2.0在线存储版

--- js/new_file.js
+++ js/new_file.js
@@ -1,184 +1,226 @@
-	function update(){//更新item数量
-				var list=document.getElementById("listdiv");
-				var childs=list.childNodes;
-				var j=0;
-				for(var i=0;i<childs.length;i++){
-					if(childs[i].childNodes[0].style.color!="green")
-					{
-						j++;
-					}
-				}
-				var items=document.getElementById("items");
-				if(j>1){
-				items.innerHTML=j+" items left";
-				}else{
-					items.innerHTML=j+" item left";
-					if(j==0){
-						document.getElementById("allbtn").style.color="green";
-					}
-				}
-				if(childs.length==0){
-					document.getElementById("allbtn").style.color="grey";
-				}
-				
-				//有已完成的项目
-				if(childs.length-j>0){
-					document.getElementById("deletecbtn").style.display="block";
-				}
-				//没有已完成的项目
-				else{
-					document.getElementById("deletecbtn").style.display="none";
-				}
-			}
-			function addlist(){//添加list
-				var listdiv=document.getElementById("listdiv");
-				var input=document.getElementById("input");
-				var compelete=document.getElementsByTagName("a")[2];
-				var text=input.value;
-				if(text.replace(/(^\s*)|(\s*$)/g, "").length!=0){
-					var newline=document.createElement("div");
-					newline.setAttribute("class","listline");
-					var newicon=document.createElement("i");
-					newicon.setAttribute("class","icon-ok-circle icon-2x");
-					newicon.setAttribute("onclick","check(this)");
-					var newtext=document.createElement("input");
-					newtext.setAttribute("type","text");
-					newtext.setAttribute("value",text);
-					newtext.setAttribute("onblur","modify(this)")
-					//newtext.setAttribute("readonly","true");
-					var deletebtn=document.createElement("div");
-					deletebtn.setAttribute("class","delete");
-					deletebtn.setAttribute("onclick","deletelist(this)");
-					deletebtn.innerHTML="x";
-					newline.appendChild(newicon);
-					newline.appendChild(newtext);
-					newline.appendChild(deletebtn);
-					listdiv.appendChild(newline);
-					document.getElementById("allbtn").style.color="grey";
-					if(compelete.className=="selected"){
-						select(compelete);
-					}
-				}
-				input.value="";		
-				update();
-			}
-			function check(obj){
-				var parent=obj.parentNode;
-				var childs=parent.childNodes;
-				var active=document.getElementsByTagName("a")[1];
-				var compelete=document.getElementsByTagName("a")[2];
-				if(obj.style.color!="green")
-				{
-					childs[1].setAttribute("class","done");
-					obj.style.color="green";
-					if(active.className=="selected"){
-						obj.parentNode.style.display="none";
-					}
 
-				}else{
-					childs[1].setAttribute("class","undone");
-					obj.style.color="grey";
-					document.getElementById("allbtn").style.color="grey";
-					if(compelete.className=="selected"){
-						obj.parentNode.style.display="none";
-					}
-				}
-				update();
+	$(document).ready(function(){
+		
+		$(function(){
+			$("#input").keypress(function(event){
+			var keycode = (event.keyCode ? event.keyCode : event.which);  
+			if(keycode==13){
+				addlist();
 			}
-			function checkall(obj){
-				var list=document.getElementById("listdiv");
-				var listchilds=list.childNodes;
-				var active=document.getElementsByTagName("a")[1];
-				var compelete=document.getElementsByTagName("a")[2];
-				if(obj.style.color!="green"){
-				for(var i=0;i<listchilds.length;i++){
-					childs=listchilds[i].childNodes;
-					childs[1].setAttribute("class","done");
-					childs[0].style.color="green";
-					obj.style.color="green";
-					}
-					if(active.className=="selected"){
-						select(active);
-					}
-					else if(compelete.className=="selected"){
-						select(compelete);
-					}
-				}else{
-						for(var i=0;i<listchilds.length;i++){
-						childs=listchilds[i].childNodes;
-						childs[1].setAttribute("class","undone");
-						childs[0].style.color="grey";
-						obj.style.color="grey";
-						}
-						if(active.className=="selected"){
-							select(active);
-						}
-						else if(compelete.className=="selected"){
-							select(compelete);
-						}
-					}
-				update();
-				}
-			function select(obj){//选择分类
-				var alist=document.getElementsByTagName("a");
-				for(var i=0;i<alist.length-1;i++){
-					alist[i].setAttribute("class","unselected");
-				}
-				obj.setAttribute("class","selected");
-				if(obj.innerHTML=="All"){
-					var lists=document.getElementsByClassName("listline");
-					for(var i=0;i<lists.length;i++){
-						lists[i].style.display="block";
-					}
-				}
-				else if(obj.innerHTML=="Active"){
-					var lists=document.getElementsByClassName("listline");
-					for(var i=0;i<lists.length;i++){
-						if(lists[i].childNodes[1].className=="done"){
-							lists[i].style.display="none";
-						}
-						else{
-							lists[i].style.display="block";
-						}
-					}
-				}
-				else if(obj.innerHTML=="Compeleted"){
-					var lists=document.getElementsByClassName("listline");
-					for(var i=0;i<lists.length;i++){
-						if(lists[i].childNodes[1].className!="done"){
-							lists[i].style.display="none";
-						}
-						else{
-							lists[i].style.display="block";
-						}
-					}
-				}
-			}
-			function deletelist(obj){//删除
-				var parent=obj.parentNode;
-				parent.parentNode.removeChild(parent);
-				update();
-			}
-			function modify(obj){//更改
-				if(obj.value==""){
-					var parent=obj.parentNode;
-					parent.parentNode.removeChild(parent);
-					update();
-				}
-			}
+			});
 			
-			//删除已完成项目
-			function clearcompeleted(){
-				var sum=0;
-				var lists=document.getElementsByClassName("listline");
-				for(var i=0;i<lists.length;i++){
-					var childs=lists[i].childNodes;
-					if(childs[0].style.color=="green"){
-						lists[i].parentNode.removeChild(lists[i]);
-						lists=document.getElementsByClassName("listline");
-						i=i-1;
+			update();
+		}).on('click','i',function(){
+			var $this = $(this);
+			
+			checkone($this);
+		}).on('click','#allbtn',function(){    		  //全选按钮
+			var $this = $(this);	
+			
+			allcheck($this);
+		}).on('click','.close_svg',function(){
+			var $this=$(this);
+			
+			deleteitem($this);
+		}).on('click','.search-type',function(){
+			if($(this).hasClass('active')){
+				return;
+			}
+			$('.search-type').removeClass('active');
+			$(this).addClass('active');
+			update();
+		}).on('click','.clear',function(){
+			clearcompleted();
+		}).on('blur','li input',function(){
+			var $this=$(this);
+			modify($this);
+		})
+		
+				
+		function addlist(){    //增加新项目
+			var item=$("#input").val();
+			if($.trim(item)==''){
+				alert("请输入正确的格式");
+			}
+			else{		
+			 if(window.localStorage) {		//支持localstorage
+			 	var list=localStorage.getItem("itemlist");
+			 	var num=parseInt(localStorage.getItem("num"));
+			 	var id=parseInt(localStorage.getItem("id"));
+			 	if(list && list!='null' ){   //localstorge不为空
+					var jsonarray = $.parseJSON(list);
+					var newitem={
+						"item"   :item,
+						"status" :"false",
+						"id"     : ++id
+					}
+					jsonarray.push(newitem);
+					localStorage.setItem("itemlist",JSON.stringify(jsonarray));
+					localStorage.setItem("num",++num);
+					localStorage.setItem("id",id);
+			 	}
+			 	else{		//localstorge为空
+			 		var itemlist=[{
+			 			"item"   : item,
+			 			"status" : "false",
+			 			"id"     : 1 
+			 		}];
+			 		localStorage.setItem("itemlist",JSON.stringify(itemlist));
+			 		localStorage.setItem("num",1);
+			 		localStorage.setItem("checknum",0);
+			 		localStorage.setItem("id",1);
+			 	}
+				}
+			$("#input").val("");
+			update();
+			}
+		}
+		
+		function update(){   //更新项目
+			var tmpl='';
+			var list=$.parseJSON(localStorage.getItem("itemlist"));
+			var num=parseInt(localStorage.getItem("num"));
+			var checknum=parseInt(localStorage.getItem("checknum"));
+			if("Active"==$('.active').html()){
+				for(var o in list){
+					if(list[o].status=="false"){
+						tmpl+='<li data-id="'+list[o].id+'">'+
+						  '<i class="icon-ok-circle icon-2x '+(list[o].status=="true"?'checked' :'')+'"></i>'+
+						  '<input type="text" value="'+list[o].item+'" class="'+(list[o].status=="true"?'done' :'')+'">'+
+						  '<img src="img/close.svg" class="close_svg">'+
+						  '</li>';
 					}
 				}
-				update();
-			
 			}
+			else if("Completed"==$('.active').html()){
+				for(var o in list){
+					if(list[o].status=="true"){
+						tmpl+='<li data-id="'+list[o].id+'">'+
+						  '<i class="icon-ok-circle icon-2x '+(list[o].status=="true"?'checked' :'')+'"></i>'+
+						  '<input type="text" value="'+list[o].item+'" class="'+(list[o].status=="true"?'done' :'')+'">'+
+						  '<img src="img/close.svg" class="close_svg">'+
+						  '</li>';
+					}
+				}
+			}
+			else{
+				for(var o in list){  
+					tmpl+='<li data-id="'+list[o].id+'">'+
+						  '<i class="icon-ok-circle icon-2x '+(list[o].status=="true"?'checked' :'')+'"></i>'+
+						  '<input type="text" value="'+list[o].item+'" class="'+(list[o].status=="true"?'done' :'')+'">'+
+						  '<img src="img/close.svg" class="close_svg">'+
+						  '</li>';
+      			}  
+      		}
+			$(".listarea ul").html(tmpl);
+			if(isNaN(num)) num=0;
+			$(".info").html(num+' items');
+			if(num==checknum&&num!=0) $("#allbtn").addClass('checked');
+			else  $("#allbtn").removeClass('checked');
+		}
+		
+		function deleteitem(obj){   //删除项目
+			var $this=obj;
+			var list=$.parseJSON(localStorage.getItem("itemlist"));
+			var num=parseInt(localStorage.getItem("num"));
+			var checknum=parseInt(localStorage.getItem("checknum"));
+			var id=$this.parent().attr("data-id");
+			for(var i in list){
+				if(list[i].id==id){
+					list.splice(i,1); 					//删除i位置的1个元素
+				}
+			}
+			localStorage.setItem("itemlist",JSON.stringify(list));
+			localStorage.setItem("num",--num);
+			if($this.siblings("i").hasClass("checked")){
+				localStorage.setItem("checknum",--checknum);
+			}
+			update();	
+		}
+		
+		function allcheck(obj){    //全选
+			var $this=obj;
+			var list=$.parseJSON(localStorage.getItem("itemlist"));
+			var num=parseInt(localStorage.getItem("num"));
+			if($this.hasClass('checked')){
+				for(var i in list){
+					list[i].status="false";
+				}
+				$this.removeClass('checked');		  //dom操作
+				localStorage.setItem("checknum",0);
+			}
+			else{
+				for(var i in list){
+					list[i].status="true";
+				}
+				$this.addClass('checked'); 	     	  //dom操作
+				localStorage.setItem("checknum",num);
+			}
+			localStorage.setItem("itemlist",JSON.stringify(list));
+			update();
+		}
+		
+		function checkone(obj){    //完成一项
+			var $this=obj;
+			if($this.attr("id")=="allbtn"){
+				return;
+			}
+			$this.toggleClass("checked");
+			$this.siblings("input").toggleClass("done");	//dom操作
+			
+			var id=$this.parent().attr("data-id");
+			var list=$.parseJSON(localStorage.getItem("itemlist"));
+			var checknum=parseInt(localStorage.getItem("checknum"));
+			for(var i in list){									//后台操作
+				if(list[i].id==id){
+					if(list[i].status=="true") {
+						list[i].status="false";
+						localStorage.setItem("checknum",--checknum);
+					}
+					else {
+						list[i].status="true";
+						localStorage.setItem("checknum",++checknum);
+					}
+				}
+			}
+			localStorage.setItem("itemlist",JSON.stringify(list));
+			update();
+		}
+		
+		function clearcompleted(){     //清除已完成
+			var list=$.parseJSON(localStorage.getItem("itemlist"));
+			var num=parseInt(localStorage.getItem("num"));
+			var checknum=parseInt(localStorage.getItem("checknum"));
+			for(var i =list.length-1;i>=0;i--){
+				if(list[i].status=="true"){
+					list.splice(i,1); 
+					num--;
+					checknum--;
+				}
+			}
+			localStorage.setItem("itemlist",JSON.stringify(list));
+			localStorage.setItem("num",num);
+			localStorage.setItem("checknum",checknum);
+			update();
+		}
+		
+		function modify(obj){            //更改内容
+			var $this=obj;
+			if($this.val()==''){
+				deleteitem($this);
+			}
+			else{
+				var id=$this.parent().attr("data-id");
+				var list=$.parseJSON(localStorage.getItem("itemlist"));
+				for(var i in list){
+					if(list[i].id==id){
+						list[i].item=$this.val();
+					}
+				}
+				localStorage.setItem("itemlist",JSON.stringify(list));
+				update();
+			}
+		}
+		
+	})
+	
